929d3cb2fe58c78db3442186e8b8507ce3a48f7c
Paige Israel <paigeaisrael@gmail.com>
2016-06-27 12:02:07 -0700

    Added index.php to the repository
    Added index.php

---
+++ index.php
@@ -0,0 +1,100 @@
+<!DOCTYPE html>
+<html lang="en">
+
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+		  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+		  <!--[if lt IE 9]>
+		  <script src="/scripts/html5shiv.min.js"></script>
+		  <![end if]-->
+        <title><?php echo $page_title; ?></title>
+		  <link rel="stylesheet" href="//code.jquery.com/ui/1.11.4/themes/smoothness/jquery-ui.css">
+	     <script src="http://code.jquery.com/jquery-1.10.2.js"></script>
+        <script src="http://code.jquery.com/ui/1.11.4/jquery-ui.js"></script>
+        <link href='http://fonts.googleapis.com/css?family=Alegreya+Sans|Alegreya|Alegreya+SC' rel='stylesheet' type='text/css' />
+        <link rel="shortcut icon" href="/images/favicon.ico" />
+        <link rel="stylesheet" href="styles/style.css"/>
+    </head>
+
+    <body>
+		  <section>  <!-- [Start the header for all pages.] -->
+				<div class="header">
+					 <a href="/pages/index.php"><img src="images/tulip-header-800x199.jpg" width="800" height="199" alt="Tulip Header" /></a>
+				</div>
+				<div class="header-text-green">
+					Paige Israel
+				</div>
+				<div class="header-medium-text-blue">
+					 Web Developer
+				</div>
+				
+				<!-- [Add navigation.] -->
+				<div class="nav ul">
+					 <nav>
+						  <ul class="ul">
+								<li><a href="/index.php">Home</a></li>
+								<li><a href="/pages/portfolio.php">Portfolio</a></li>
+								<li><a href="/pages/contact.php">Contact</a></li>
+								<li><a href="https://www.linkedin.com/in/paigeisrael">LinkedIn</a></li>
+								<li><a href="https://github.com/andreaturner/andreaturner">GitHub</a></li>
+						  </ul>
+					 </nav> 
+				</div>  <!-- [End navigation]. -->
+		  </section> <!-- [End the header section for all pages.] -->
+<!--[Start content] -->
+
+	 <section>
+		  <div class="header-text-blue">
+				Who I am and what I do....
+		  </div>
+		  <div class="pic">
+				<img src="images/picture-300x301.jpg" width="300" height="301" alt="Photo of Paige Israel">
+		  </div>
+		  <div class="text-blue-index">
+				I am a kind, quirky, hard-working perfectionist who is passionate about giving people the opportunity
+				to express themselves and showcase thier work or business via the World Wide Web using my talent 
+				and skill as a Web Developer.
+				<br>
+				Over the course of my career in IT, I've learned that my particular area of expertise lies in web design and development.
+				Having worked in multiple fast-paced, deadline-driven environments, I have a proven track record of consistently delivering complex
+				projects on time and to budget. I've played a key role in the development of key features for client and internal websites, including
+				WordPress migrations, form/document integrations, and user experience improvements. In every web design project I've managed, I've
+				consistently exceeded performance metrics when it comes to business requirements and client expectations. Furthermore, I possess
+				exceptional time management, critical thinking, and organizational skills that enable me to excel in demanding work conditions.
+		  </div>
+	 </section>
+	 
+	 <section>
+		  <br>
+		  <br>
+		  
+		  <div class="header-text-green-index">
+				I have experience with:
+		  </div>
+		  <div class="text-blue-index-list">
+		  		 <ul>
+						  <li>HTML</li>
+						  <li>HTML5</li>
+						  <li>CSS</li>
+						  <li>CSS3</li>
+					 	  <li>JavaScript</li>
+						  <li>jQuery</li>
+					 	  <li>GitHub</li>
+						  <li>Subversion</li>
+						  <li>WordPress</li>
+						  <li>Photoshop</li>
+					 </ul>
+		  </div>
+	 
+		  
+	 </section>
+		  
+		  
+		  
+	 
+            
+		    
+        
+    </body>
+        
+</html>
